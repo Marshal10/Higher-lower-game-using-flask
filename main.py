01bde64176a638b6787f791bb34dd6d026d4d520
Marshal10 <marshalmaleane123@gmail.com>
2024-01-13 23:00:24 +0530

    Flow of program

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from random import randint
 
 app=Flask(__name__)
-
+random_number=randint(0,9)
 
 @app.route("/")
 def hello():
@@ -9,6 +10,16 @@ def hello():
             '<img src="https://media0.giphy.com/media/grDFHLDd6Bl9vDCr4Z/200w.webp?cid=ecf05e47faa08eizq9bjb1cj4ss8nafs33ypy7stcooqp0gw&ep=v1_gifs_search&rid=200w.webp&ct=g" width="500" height="500">'
 
 
+@app.route("/<int:num>")
+def guess(num):
+    if num==random_number:
+        pass
+    elif num<random_number:
+        pass
+    elif num>random_number:
+        pass
+    else:
+        return "<h1>Please Type in a number only!</h1>"
 
 if __name__=="__main__":
     app.run(debug=True)
